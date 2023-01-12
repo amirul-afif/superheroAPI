e34a44ace2944e900bf445dbf8b50d4d2301cd9c
Amirul Afif <44400302+amirul-afif@users.noreply.github.com>
2023-01-12 22:58:29 +0000

    - combine search and random method in 1 function

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const BASE_URL = `https://superheroapi.com/api.php/${TOKEN}`
 // HTML div
 const newHeroButtonDiv = document.getElementById('newHeroButton')
 const heroImageDiv = document.getElementById('heroImage')
+const searchButtonDiv = document.getElementById('searchButton')
+const inputDiv = document.getElementById('input')
 
 // get random hero's ID
 const getRandomHeroID = () => {
@@ -12,18 +14,59 @@ const getRandomHeroID = () => {
   return Math.ceil(Math.random() * numOfHeroes)
 }
 
-// fetch the data from the API
-const getSuperHero = (id) => {
-  fetch(`${BASE_URL}/${id}`)
-    .then(response => response.json())
-    .then(json => {
-      const name = json.name
-      const img = json.image.url
-      heroImageDiv.innerHTML =
-        `<h3>${name}</h3> <img src='${img}' height=400/>`
-      console.log(json)
-    })
+// // fetch the data from the API
+// const getSuperHero = (id) => {
+//   fetch(`${BASE_URL}/${id}`)
+//     .then(response => response.json())
+//     .then(json => {
+//       const name = json.name
+//       const img = json.image.url
+//       heroImageDiv.innerHTML =
+//         `<h3>${name}</h3> <img src='${img}' height=400/>`
+//       console.log(json)
+//     })
+// }
+
+// // search for a super hero
+// const getSearchSuperHero = (name) => {
+//   fetch(`${BASE_URL}/search/${name}`)
+//     .then(response => response.json())
+//     .then(json => {
+//       const hero = json.results[0]
+//       const name = hero.name
+//       const img = hero.image.url
+//       heroImageDiv.innerHTML = `<h3>${name}</h3> <img src='${img}' height=400/>`
+//       console.log(json)
+//     })
+// }
+
+const getSuperHeroMain = (name, id) => {
+  if (name != '') {
+    fetch(`${BASE_URL}/search/${name}`)
+      .then(response => response.json())
+      .then(json => {
+        const hero = json.results[0]
+        const name = hero.name
+        const img = hero.image.url
+        heroImageDiv.innerHTML = `<h3>${name}</h3> <img src='${img}' height=400/>`
+        console.log(json)
+      })
+  } else if (id != '') {
+    fetch(`${BASE_URL}/${id}`)
+      .then(response => response.json())
+      .then(json => {
+        const name = json.name
+        const img = json.image.url
+        heroImageDiv.innerHTML =
+          `<h3>${name}</h3> <img src='${img}' height=400/>`
+        console.log(json)
+      })
+  }
 }
 
 // run the program everytime the button is clicked
-newHeroButtonDiv.onclick = () => getSuperHero(getRandomHeroID())
+// newHeroButtonDiv.onclick = () => getSuperHero(getRandomHeroID())
+// searchButtonDiv.onclick = () => getSearchSuperHero(inputDiv.value)
+
+newHeroButtonDiv.onclick = () => getSuperHeroMain('', getRandomHeroID())
+searchButtonDiv.onclick = () => getSuperHeroMain(inputDiv.value, '')
