12733ead8de879e5b99ad781c06268f3f71a5c66
Amirul Afif <44400302+amirul-afif@users.noreply.github.com>
2023-01-12 23:51:27 +0000

    100% working with all functionalities

--- script.js
+++ script.js
@@ -40,33 +40,50 @@ const getRandomHeroID = () => {
 //     })
 // }
 
-const getSuperHeroMain = (name, id) => {
+const getSuperHero = (name, id) => {
+  // using search input
   if (name != '') {
     fetch(`${BASE_URL}/search/${name}`)
       .then(response => response.json())
       .then(json => {
         const hero = json.results[0]
-        const name = hero.name
-        const img = hero.image.url
-        heroImageDiv.innerHTML = `<h3>${name}</h3> <img src='${img}' height=400/>`
+        showHeroInfo(hero)
+
         console.log(json)
       })
+    // random heroes
   } else if (id != '') {
     fetch(`${BASE_URL}/${id}`)
       .then(response => response.json())
       .then(json => {
-        const name = json.name
-        const img = json.image.url
-        heroImageDiv.innerHTML =
-          `<h3>${name}</h3> <img src='${img}' height=400/>`
+        const hero = json
+        showHeroInfo(hero)
+
         console.log(json)
       })
   }
 }
 
-// run the program everytime the button is clicked
-// newHeroButtonDiv.onclick = () => getSuperHero(getRandomHeroID())
-// searchButtonDiv.onclick = () => getSearchSuperHero(inputDiv.value)
+const statToEmoji = {
+  intelligence: '🧠',
+  strength: '💪',
+  speed: '⚡',
+  durability: '🏋️‍♂️',
+  power: '📊',
+  combat: '⚔️',
+}
 
-newHeroButtonDiv.onclick = () => getSuperHeroMain('', getRandomHeroID())
-searchButtonDiv.onclick = () => getSuperHeroMain(inputDiv.value, '')
+const showHeroInfo = (character) => {
+  const name = `<h2>${character.name}</h2>`
+  const img = `<img src='${character.image.url}' height=400/>`
+  
+  const stats = Object.keys(character.powerstats).map(stat => {
+    return `<p>${statToEmoji[stat]} ${stat.toUpperCase()}: ${character.powerstats[stat]}</p>`
+  }).join('')
+  
+  heroImageDiv.innerHTML = `${name}${img}${stats}`
+}
+
+// run the program everytime the button is clicked
+newHeroButtonDiv.onclick = () => getSuperHero('', getRandomHeroID())
+searchButtonDiv.onclick = () => getSuperHero(inputDiv.value, '')
